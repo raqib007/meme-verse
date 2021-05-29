e2d0175d80a5a3c7f99cd6203e7ffb4f6b3a01cf
raqib007 <rhsabbir@yahoo.com>
2021-05-29 14:37:15 +0600

    Modify swagger.json file

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const controller = require("../controllers/comment.controller");
+const { authJwt } = require("../middlewares");
 
 router.get("", controller.getAll);
-router.get("/:comment_id", controller.findOne);
-router.post("", controller.create);
-router.put("/:comment_id",  controller.update);
-router.delete("/:comment_id",  controller.delete);
+router.get("/:comment_id", [authJwt.verifyToken], controller.findOne);
+router.post("",[authJwt.verifyToken], controller.create);
+router.put("/:comment_id", [authJwt.verifyToken], controller.update);
+router.delete("/:comment_id", [authJwt.verifyToken], controller.delete);
 
 module.exports = router;
