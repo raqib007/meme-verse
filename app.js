3089db8577500d5686b704c2b21014e0de604e37
raqib007 <rhsabbir@yahoo.com>
2021-05-29 15:27:08 +0600

    new build file

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ server.listen(PORT, () => {
 });
 
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
