0811494e28270c918594829e79d2c5287357c3ff
raqib007 <rhsabbir@yahoo.com>
2021-05-29 21:17:29 +0600

    Header Refactored

--- client/src/components/AppHeader/AppHeader.js
+++ client/src/components/AppHeader/AppHeader.js
@@ -1,14 +1,27 @@
 import React, {useContext} from 'react';
-import {Button, Layout, Avatar} from "antd";
+import {Button, Layout, Avatar,Menu, Dropdown } from "antd";
 import "./AppHeader.css";
 import {Link} from "react-router-dom";
 import {AuthContext} from "../../context-provider/userContext";
 import logo from "../../assets/img/logo.png";
-import {UserOutlined, UploadOutlined} from '@ant-design/icons';
+import {UserOutlined, UploadOutlined, BarsOutlined} from '@ant-design/icons';
 
 export default function AppHeader(props) {
     const auth = useContext(AuthContext);
     const {Header} = Layout;
+
+    const menu1 = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item onClick={() => props.showModal(true,'signin','Sign In')}>Login</Menu.Item>
+            <Menu.Item onClick={() => props.showModal(true,'signup','Sign Up')}>Sign Up</Menu.Item>
+        </Menu>
+    );
+    const menu2 = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item onClick={() => props.showModal(true,'signin','Sign In')}>Logout</Menu.Item>
+        </Menu>
+    );
+
     function handleLogoutClick(){
         if(localStorage.getItem('token')){
             localStorage.removeItem("token");
@@ -17,13 +30,30 @@ export default function AppHeader(props) {
             })
         }
     }
+    function handleMenuClick(e) {
+        console.log('click', e);
+    }
 
     return (
         <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
             <div className="header-content">
                 <Link to={"/"}><img className="logo" src={logo} alt=""/></Link>
+
+                <span className="headerMenu">
+                    {auth.user !== null ?
+                    ( 
+                        <Dropdown.Button overlay={menu2}></Dropdown.Button>
+                        ) :(
+                        <Dropdown.Button overlay={menu1}></Dropdown.Button>
+                    )}
+                    {/* <BarsOutlined/> */}
+                </span>
+                
+                
+
                 {auth.user !== null ?
-                ( <div className="header-button">
+                ( 
+                    <div className="header-button">
                         <Avatar icon={<UserOutlined />} /> &nbsp;&nbsp;
                         <span style={{color:'#ffffff',marginRight:10}}>{auth.user.first_name}</span>
                     <Button type="primary" onClick={() => props.showModal(true,'upload','Create Post')}
